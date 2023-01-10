5f10ac8c78a3a09ef903ea69d44e37c6bdb1ef6d
Lennie Nurse <lnursej@gmail.com>
2023-01-10 10:13:50 -0500

    "update"

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,5 +1,20 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
-export default function Transaction() {
-  return <div>Transaction</div>
+export default function Transaction({ transaction, index }) {
+  return (
+    <div className="detail">
+      <tr>
+        <td>{transaction.date}</td>
+      </tr>
+      <tr>
+        <td>
+          <Link to={`/transactions/${index}`}>{transaction.item_name}</Link>
+        </td>
+      </tr>
+      <tr>
+        <td>{transaction.amount}</td>
+      </tr>
+    </div>
+  )
 }
